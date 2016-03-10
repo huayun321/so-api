a0e9020524d93a354a9034d5edd3259bc9765918
huayun321 <huayun321@gmail.com>
2016-03-10 16:45:43 +0800

    完成四个主要接口的文档

--- server.js
+++ server.js
@@ -8,6 +8,33 @@ io.on('connection', function (socket) {
         console.log('I received a private message by ', from, ' saying ', msg);
     });
 
+    /**
+     * 创建房间
+     * 参数: 人数限制(可选)
+     * 返回值: 房间序号
+     * 如有人数限制,则在达到人数限制时,自动群发当前房间所有人的序列号
+     */
+
+
+    /**
+     * 加入房间
+     * 参数: 房间名称 消息对象
+     * 返回值: 房间人数 所在房间人中的第几人
+     * 群发包含加入者个人信息的加入事件
+     */
+
+
+    /**
+     * 群发一个消息到指定房间的所有人
+     *
+     */
+
+
+    /**
+     * 请求发送指定房间内所有人的序号
+     */
+
+
     socket.on('disconnect', function () {
         io.emit('user disconnected');
     });
