367f5bbb1c65bb3c8850b2306727c6ac0d7c1033
huayun321 <huayun321@gmail.com>
2016-03-16 17:12:57 +0800

    添加underscore包 添加API错误处理的实例

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 var io = require('socket.io')(5000);
 //uuid
 var uuid = require('uuid');
+//underscore
+var _ = require('underscore');
 
 io.on('connection', function (socket) {
     console.log(socket.id);
@@ -17,6 +19,12 @@ io.on('connection', function (socket) {
      * 如有人数限制,则在达到人数限制时,自动群发当前房间所有人的序列号
      */
     socket.on('create room', function(cb) {
+        //check if there is a cb
+        if( !_.isFunction(cb) ) {
+            socket.emit('room error', 'create room need function as first arg');
+            return;
+        }
+
         //generate an uuid for room name
         // Generate a v1 (time-based) id
         var roomName = uuid.v1(); // -> '6c84fb90-12c4-11e1-840d-7b25c5ee775a'
@@ -25,10 +33,8 @@ io.on('connection', function (socket) {
         socket.join(roomName);
 
         //callback with room name
-        //check if there is a cb
-        if(cb) {
-            cb(roomName);
-        }
+        cb(roomName);
+
     });
 
 
@@ -39,9 +45,26 @@ io.on('connection', function (socket) {
      * 群发包含加入者个人信息的加入事件
      * 如有人数限制,则在达到人数限制时,自动群发当前房间所有人的序列号
      */
-    socket.on('join room', function(roomName, msgObj, size, cb) {
+    socket.on('join room', function(opts, cb) {
+        //check if opts is obj
+        if( !_.isObject(opts) ) {
+            return;
+        }
+        //check if opts.roomName is string
+        if( !_.isString(opts.roomName) ) {
+            return;
+        }
+        //check if opts.roomName is uuid
+
+        //check cb is function
+        if( !_.isFunction(cb) ) {
+            return;
+        }
+
         //test
-        console.log('join room, roomName:', roomName);
+        console.log('join room, opts:', opts);
+        cb(opts);
+        console.log(cb);
 
 
         //check if arg size set
@@ -49,7 +72,6 @@ io.on('connection', function (socket) {
                 //if smaller than join the room of roomName
                 //else reject the client with a msg
             //if clients length is equal to size
-                //join the room of roomName
                 //emit an event with each index of all clients in roomName
             //else join the room of roomName
 
@@ -57,6 +79,8 @@ io.on('connection', function (socket) {
             //join the room of roomName
 
         //emit an event with current client's msgObj
+
+
 
     });
 
