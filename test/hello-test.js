e8cb7485022276e2390195b32e1eb13fefdc21d5
huayun321 <huayun321@gmail.com>
2016-03-29 12:33:13 +0800

    初始化socket.io mocha测试环境

--- test/hello-test.js
+++ test/hello-test.js
@@ -1,12 +1,15 @@
-var io = require('socket.io-client');
-
-var server_url = 'http://localhost:5000';
+var should = require('chai').should()
+    ,io = require('socket.io-client')
+    ,server_url = 'http://localhost:5000';
 
 describe('conn', function () {
     it('user can connect to server and receive msg from server', function(done) {
        var client = io.connect(server_url);
         client.on('this', function(msg) {
-            console.log(msg);
+            should.exist(msg);
+            msg.should.have.property('will').equal('be received by everyone');
+            msg.should.be.an('object');
+            msg.will.should.equal('be received by everyone');
             done();
         })
     });
